3402edac03300d79cadebe07fa3554f4e3f05e52
doanhuyphuong <124856427+doanhuyphuong@users.noreply.github.com>
2023-02-24 10:14:42 +0700

    Add files via upload

---
+++ slide35.java
@@ -0,0 +1,14 @@
+package BaiThucHanh2202;
+import java.util.Scanner;
+public class slide35{
+public static void main (String[]args){
+    Scanner sc =new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    if (a>=b){
+        System.out.print("a lon hon hoac bang b");
+    }else{
+        System.out.print("a nho hon b")
+    }
+}
+}
