d12b15fa68c1cf50de47316147bcffaa6efb45fa
doanhuyphuong <124856427+doanhuyphuong@users.noreply.github.com>
2023-04-23 19:55:52 +0700

    Add files via upload

---
+++ thuchanhjava/BaiThucHanhLab3/src/BaiThucHanh1304/Shapes/HinhVuong.java
@@ -0,0 +1,14 @@
+package Shapes;
+
+import java.util.Scanner;
+
+public class HinhVuong extends HinhChuNhat {
+    public HinhVuong(){
+        ten="Hinh Vuong";
+    }
+    public void nhapCanh(){
+        System.out.println("Canh=");
+        Scanner sc=new Scanner(System.in);
+        dai=rong=sc.nextFloat();
+    }
+}
